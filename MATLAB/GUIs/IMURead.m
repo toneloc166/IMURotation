7959cfdc04b4cd567d3df1a02fc0d2e1316e96f7
toneloc166 <antoniozuniga31@yahoo.com>
2018-07-15 17:22:36 -0400

    Added initial calibration parameters for startup/PORs

--- MATLAB/GUIs/IMURead.m
+++ MATLAB/GUIs/IMURead.m
@@ -77,8 +77,8 @@ varargout{1} = handles.output;
 % This script reads and displays the IMU data for a particular object
 
 % Create serial object and connect device
-s1 = serial('COM3','Baudrate',115200);
-s2 = serial('COM4','Baudrate',115200);
+s1 = serial('COM4','Baudrate',115200);
+s2 = serial('COM5','Baudrate',115200);
 s1.Terminator = 10;
 s2.Terminator = 10; 
 fopen(s1);
@@ -101,38 +101,51 @@ fopen(s2);
 % h(6) = surf(-X,-Y,-Z);
 
 % Rectangular Prism Model (iphone case)
-[X1,Y1] = meshgrid(linspace(0,2,20),linspace(0,3,20));
-[X2,Y2] = meshgrid(linspace(0,2,20),linspace(0,6,20));
-[X3,Y3] = meshgrid(linspace(0,3,20),linspace(0,6,20));
-Z11 = zeros(length(X1),length(Y1));
-Z21 = zeros(length(X2),length(Y2));
-Z31 = zeros(length(X3),length(Y3));
-Z12 = 6 * ones(length(X1),length(Y1));
-Z22 = 3 * ones(length(X2),length(Y2));
-Z32 = 2 * ones(length(X3),length(Y3));
-h(1) = surf(Z21,Y2,X2);
+% Axis of rotation initializes to X,Y,Z = 0,0,0
+front = imread('front1.jpg');
+back = imread('back1.jpg');
+smallside = imread('smallside1.jpg');
+smallsideflip = flip(smallside,1);
+bigside = imread('bigside1.jpg');
+bigsideflip = flip(bigside,1);
+[X1,Y1] = meshgrid(linspace(-1.8,0,20),linspace(-1.65,1.65,20));
+[X2,Y2] = meshgrid(linspace(-1.8,0,20),linspace(-3.05,3.05,20));
+[X3,Y3] = meshgrid(linspace(-1.65,1.65,20),linspace(-3.05,3.05,20));
+Z11 = -3.05 * ones(length(X1),length(Y1));
+Z21 = -1.65 * ones(length(X2),length(Y2));
+Z31 = -1.8 * ones(length(X3),length(Y3));
+Z12 = 3.05 * ones(length(X1),length(Y1));
+Z22 = 1.65 * ones(length(X2),length(Y2));
+Z32 = zeros(length(X3),length(Y3));
+h(1) = surf(Z21,Y2,X2, "FaceColor", "texturemap", "CData", bigsideflip,...
+    "FaceAlpha", 1, "EdgeColor", "none");
 hold on;
-h(2) = surf(Y1,Z11,X1);
-h(3) = surf(X3,Y3,Z31);
-h(4) = surf(Y1,Z12,X1);
-h(5) = surf(X3,Y3,Z32);
-h(6) = surf(Z22,Y2,X2);
+h(2) = surf(Y1,Z11,X1, "FaceColor", "texturemap", "CData", smallside,...
+    "FaceAlpha", 1, "EdgeColor", "none");
+h(3) = surf(X3,Y3,Z31, "FaceColor", "texturemap", "CData", back,...
+    "FaceAlpha", 1, "EdgeColor", "none");
+h(4) = surf(Y1,Z12,X1, "FaceColor", "texturemap", "CData", smallsideflip,...
+    "FaceAlpha", 1, "EdgeColor", "none");
+h(5) = surf(X3,Y3,Z32, "FaceColor", "texturemap", "CData", front,...
+    "FaceAlpha", 1, "EdgeColor", "none");
+h(6) = surf(Z22,Y2,X2, "FaceColor", "texturemap", "CData", bigside,...
+    "FaceAlpha", 1, "EdgeColor", "none");
 
 % Cylinder with cone (probe device)
+% Axis of rotation initializes to X,Y,Z = 0,0,0
 % Cylinder
 r1 = 0.2;
 h1 = 6;
 [x1,y1,z1] = cylinder(r1);
-x1 = x1 - 2;
 z1 = z1 * h1;
 
 % Cone
-h2 = 2;
+h2 = 1;
 [x2,y2,z2] = cylinder([r1,0]);
-x2 = x2 - 2;
+z2 = z2 + 6;
 z2 = z2 * h2;
 
-probe(1) = surf(x1,y1,z1, "Facecolor", [0.75,0.75,0.75]);
+probe(1) = surf(x1,y1,-z1, "Facecolor", [0.4,0.4,0.4]);
 probe(2) = surf(x2,y2,-z2, "Facecolor", [0.75,0.75,0.75]);
 
 % Grid setup
@@ -153,7 +166,7 @@ set(probe, "parent", tform2);
 t = 0:0.1:2000*pi;
 X = 4*cos(t/1.6);
 Y = 4*sin(t/1.6);
-Z = 0;
+Z = 2*sin(t/1.6);
 idx = 1;
 
 while (1)
@@ -188,20 +201,25 @@ while (1)
         % Apply rotation and translation
 %         dcm1(1,4) = 0.7 * X(idx);
 %         dcm1(2,4) = 0.7 * Y(idx);
+%         dcm1(3,4) = 0.7 * Z(idx);
+%         dcm2(1,4) = 0.7 * (X(idx));
+%         dcm2(2,4) = 0.7 * (Y(idx));
+%         dcm2(3,4) = 0.7 * (Z(idx));
+
         set(tform1,"Matrix",dcm1);
         set(tform2,"Matrix",dcm2);
         idx = idx + 1;
         
         % Calibration parameters
-        set(handles.system, "string", calArray1(1));
+        % set(handles.system, "string", calArray1(1));
         set(handles.gyroscope, "string", calArray1(2));
         set(handles.accelerometer, "string", calArray1(3));
-        set(handles.magnetometer, "string", calArray1(4));
+        % set(handles.magnetometer, "string", calArray1(4));
         
-        set(handles.systemObject, "string", calArray2(1));
+        % set(handles.systemObject, "string", calArray2(1));
         set(handles.gyroscopeObject, "string", calArray2(2));
         set(handles.accelerometerObject, "string", calArray2(3));
-        set(handles.magnetometerObject, "string", calArray2(4));
+        % set(handles.magnetometerObject, "string", calArray2(4));
         
         drawnow;
         
